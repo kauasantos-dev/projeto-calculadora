fdc8e350c7a4fb43e583daef0b8d5f2635022183
Kaua Santos <kavillykaua@gmail.com>
2026-01-27 12:41:18 -0300

    refactor: remove o botão de igualdade da classe BotoesPontoIgual

--- interface/botoes_ponto_igual.py
+++ interface/botoes_ponto_igual.py
@@ -21,19 +21,3 @@ class BotoesPontoIgual:
             relwidth=0.14,
             relheight=0.09
         )
-
-        self.igual = Button(
-            self.aplicacao,
-            text="=", font=("Arial", 20),
-            bg="orange",
-            fg="white",
-            bd=1,
-            relief="raised"
-        )
-
-        self.igual.place(
-            relx=0.40,
-            rely=0.60,
-            relwidth=0.14,
-            relheight=0.09
-        )
