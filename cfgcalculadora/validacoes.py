eafca2aa35fa9b1cd90ce77d44edea96d90c993e
Kaua Santos <kavillykaua@gmail.com>
2026-02-05 16:59:06 -0300

    refactor: ajusta regex para incluir operadores de potência e radiciação

--- cfgcalculadora/validacoes.py
+++ cfgcalculadora/validacoes.py
@@ -4,12 +4,15 @@ from .uteis import realizar_operacao
 def validar_operacao(valor, operacao_atual):
     if valor is None:
         return "0"
+    
     elif operacao_atual == "0":
         return valor
+    
     elif valor == "=":
-        estrutura_operacao = r'^[-+]?[0-9]+(\.[0-9]+)?([-+×÷][0-9]+(\.[0-9]+)?)+$'
+        estrutura_operacao = r'^[-+]?[0-9]+(\.[0-9]+)?([\-+×÷\^\√][0-9]+(\.[0-9]+)?)+$'
         if re.fullmatch(estrutura_operacao, operacao_atual):
             return str(realizar_operacao(operacao_atual))
         return "0"
+    
     else:
         return None
