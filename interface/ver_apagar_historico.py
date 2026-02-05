860451936eb710becd7e8d2e7c9cf12d766a73eb
Kaua Santos <kavillykaua@gmail.com>
2026-02-05 13:59:42 -0300

    refactor: ajusta tamanho, posição e estilo dos botões de gerenciamento de histórico

--- interface/ver_apagar_historico.py
+++ interface/ver_apagar_historico.py
@@ -1,60 +1,57 @@
-from tkinter import Button, Tk
+from customtkinter import CTkButton, CTk
 from .display_resultado import DisplayResultadoOperacoes
 
 class GerenciarHistorico:
-    def __init__(self, aplicacao: Tk, display_resultados: DisplayResultadoOperacoes):
+    def __init__(self, aplicacao: CTk, display_resultados: DisplayResultadoOperacoes):
         self.aplicacao = aplicacao
         self.display_resultados = display_resultados
     
     def historico_operacoes(self):
-        self.ver_historico = Button(
-            self.aplicacao,
-            text="Ver Histórico", 
-            font=("Arial", 12),
-            background="orange", 
-            fg="white",
-            bd=1,
-            relief="raised"
-        )
-
-        self.ver_historico.place(
-            relx=0.61,
-            rely=0.70,
-            relwidth=0.29,
-            relheight=0.09
-        )
-
-        self.apagar_historico = Button(
-            self.aplicacao,
-            text="Apagar Histórico", 
-            font=("Arial", 12),
-            background="orange", 
-            fg="white",
-            bd=1,
-            relief="raised"
+        self.apagar_historico = CTkButton(
+            master=self.aplicacao,
+            text='Apagar Histórico',
+            text_color='white',
+            font=('Arial', 16),
+            fg_color='orange', 
+            corner_radius=30
         )
 
         self.apagar_historico.place(
             relx=0.1,
-            rely=0.70,
-            relwidth=0.44,
+            rely=0.66,
+            relwidth=0.38,
             relheight=0.09
         )
 
-        self.apagar_display = Button(
-            self.aplicacao,
+        self.ver_historico = CTkButton(
+            master=self.aplicacao,
+            text="Ver Histórico", 
+            text_color='white',
+            font=("Arial", 16),
+            fg_color="orange", 
+            corner_radius=30
+        )
+
+        self.ver_historico.place(
+            relx=0.67,
+            rely=0.66,
+            relwidth=0.25,
+            relheight=0.09
+        )
+
+        self.apagar_display = CTkButton(
+            master=self.aplicacao,
             text="Limpar",
-            font=("Arial", 12),
-            background="orange",
-            fg="white",
-            bd=1,
-            relief="raised",
+            text_color='white',
+            font=("Arial", 16),
+            fg_color="orange",
+            corner_radius=30,
             command=lambda: self.display_resultados.inserir(None)
         )
 
         self.apagar_display.place(
-            relx=0.61,
-            rely=0.60,
-            relwidth=0.29,
-            relheight=0.09
+            relx=0.23,
+            rely=0.57,
+            relwidth=0.25,
+            relheight=0.08
         )
