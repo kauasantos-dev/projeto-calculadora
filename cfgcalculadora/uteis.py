278970c1a2338a4b2c9fa02b8107bfba51dad817
Kaua Santos <kavillykaua@gmail.com>
2026-02-10 18:51:25 -0300

    refactor: adiciona comentários para melhorar compreensão da lógica da função realizar_operacao

--- cfgcalculadora/uteis.py
+++ cfgcalculadora/uteis.py
@@ -20,42 +20,45 @@ def apagar_historico():
     return True
 
 def realizar_operacao(operacao_usuario: str):
-    numeros_operadores_separados = []
+    numeros_e_operadores_separados = []
     primeiro_elemento_operacao = operacao_usuario[0]
-        
     valores_da_operacao = ""
+
+    # Separa + ou - do início da operação, garantindo que o primeiro índice seja um número
     if primeiro_elemento_operacao in "+-":
         valores_da_operacao = primeiro_elemento_operacao
         operacao_usuario = operacao_usuario[1:]
 
+    # Separa números e operadores para serem identificados individualmente na lista
     for elemento in operacao_usuario:
         if elemento.isdigit() or elemento == ".":
             valores_da_operacao += elemento
         else:
-            numeros_operadores_separados.append(valores_da_operacao)
-            numeros_operadores_separados.append(elemento)
+            numeros_e_operadores_separados.append(valores_da_operacao)
+            numeros_e_operadores_separados.append(elemento)
             valores_da_operacao = ""
-    numeros_operadores_separados.append(valores_da_operacao)
-        
-    if len(numeros_operadores_separados) == 3:
-        valores = [float(numeros_operadores_separados[0]), float(numeros_operadores_separados[2])]
-        operador = numeros_operadores_separados[1]
+    numeros_e_operadores_separados.append(valores_da_operacao)
+    
+    if len(numeros_e_operadores_separados) == 3:
+        valores = [float(numeros_e_operadores_separados[0]), float(numeros_e_operadores_separados[2])]
+        operador = numeros_e_operadores_separados[1]
         resultado_operacao = chamar_operacao(valores, operador)
         return resultado_operacao
-        
+    
     resultado_operacao_anterior = None
-    for valor in range(1, len(numeros_operadores_separados), 2):
-        if resultado_operacao_anterior or resultado_operacao_anterior == 0:
+    # Avança de 2 em 2 porque a lista alterna entre operando e operador
+    for valor in range(1, len(numeros_e_operadores_separados), 2):
+        if resultado_operacao_anterior != None:
             primeiro_valor = resultado_operacao_anterior
         else:
-            primeiro_valor = float(numeros_operadores_separados[valor-1])
-
-        segundo_valor = float(numeros_operadores_separados[valor+1])
+            primeiro_valor = float(numeros_e_operadores_separados[valor-1])
+        segundo_valor = float(numeros_e_operadores_separados[valor+1])
         total_numeros = [primeiro_valor, segundo_valor]
-        operador_equacao = numeros_operadores_separados[valor]
-        resultado_operacao_anterior = chamar_operacao(total_numeros, operador_equacao)
+        operador_da_equacao = numeros_e_operadores_separados[valor]
+        resultado_operacao_anterior = chamar_operacao(total_numeros, operador_da_equacao)
 
-        if valor == len(numeros_operadores_separados) - 2:
+        # Se for o penúltimo índice, significa que é a última operação
+        if valor == len(numeros_e_operadores_separados) - 2:
             break
     resultado_final_operacao = resultado_operacao_anterior
     return resultado_final_operacao
@@ -80,3 +83,10 @@ def chamar_operacao(numeros: list, simbolo_operacao: str):
         resultado_operacao = operacoes.potenciacao(numeros)
     
     return resultado_operacao
+
+def salvar_operacao_no_historico(operacao_efetuada, resultado):
+    nova_operacao = {operacao_efetuada: resultado}
+
+    historico_salvo = gerenciar_arquivos.arquivo_r()
+    historico_salvo.append(nova_operacao)
+    gerenciar_arquivos.arquivo_w(historico_salvo)
